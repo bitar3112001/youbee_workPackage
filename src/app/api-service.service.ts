adf60d1a47957332b4d685da99ba09a5a85ab6dd
bitar3112001 <141562620+bitar3112001@users.noreply.github.com>
2024-07-29 17:01:43 +0300

    serv2

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -14,7 +14,6 @@ export class ApiServiceService {
   constructor(private http: HttpClient) { }
 
   private getToken(): string {
-    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0IiwicGVybWlzc2lvbnMiOlsiZ2V0OmFzc2V0cy9kZXYiLCJnZXQ6ZXNnLWRhdGEvZGV0YWlscyJdLCJleHAiOjE3MjM5NzE1NTh9.MQjfAk-nc4p3tqNOeb9bscCadR1CLJO2Df1nriIFw7g";
     const token = environment.apiToken
     return token;
   }
