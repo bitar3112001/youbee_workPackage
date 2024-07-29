53a6cd7470a99ebb1e061b339a41472666b60ae4
bitar3112001 <141562620+bitar3112001@users.noreply.github.com>
2024-07-29 19:45:36 +0300

    last

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
 import { AssetData } from './model/AssetData';
-import { environment } from '../../environment/environment';
+import { environment } from '../environment/environment';
 
 @Injectable({
   providedIn: 'root'
---
+++ src/environment/environment.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+  production: false,
+  apiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0IiwicGVybWlzc2lvbnMiOlsiZ2V0OmFzc2V0cy9kZXYiLCJnZXQ6ZXNnLWRhdGEvZGV0YWlscyJdLCJleHAiOjE3MjM5NzE1NTh9.MQjfAk-nc4p3tqNOeb9bscCadR1CLJO2Df1nriIFw7g',
+
+
+};
